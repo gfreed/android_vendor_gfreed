6da11b050e6d35e1884e4901e1226d6e9267a263
Rene Treffer <treffer@measite.de>
2014-01-23 23:09:24 +0100

    BRAND may not contain spaces

--- products/gofree_arm.mk
+++ products/gofree_arm.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 PRODUCT_NAME  := gofree_arm
-PRODUCT_BRAND := GoFree Android
+PRODUCT_BRAND := Android
 PRODUCT_MODEL := GoFree on ARM Emulator
 
