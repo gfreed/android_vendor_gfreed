db3c3a353a1cb805ca1ff4012dba7447c17e1494
Rene Treffer <treffer@measite.de>
2014-01-25 11:04:51 +0100

    Add definitions to android build

--- products/gofree_arm.mk
+++ products/gofree_arm.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
+$(call inherit-product, vendor/gofree/config/common.mk)
+
 PRODUCT_NAME  := gofree_arm
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := GoFree on ARM Emulator
