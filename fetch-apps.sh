a8f27df200b3d816e38441d5d511f946597f79eb
Rene Treffer <treffer@measite.de>
2014-01-26 16:43:22 +0100

    Write a Android.mk for fetched packages

--- fetch-apps.sh
+++ fetch-apps.sh
@@ -7,3 +7,22 @@ function fetch {
 }
 
 fetch 'https://f-droid.org/FDroid.apk'
+
+echo LOCAL_PATH := \$\(call my-dir\) > prebuilt/apps/Android.mk
+
+ls prebuilt/apps/*.apk | while read FILE ; do
+  APK=$(echo $FILE | sed 's:.*/::')
+  NAME=$(echo $APK | sed 's:/[.]apk::')
+  cat >> prebuilt/apps/Android.mk << EOF
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := $NAME
+LOCAL_MODULE_OWNER := gofree
+LOCAL_SRC_FILES := common/app/\$(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+include \$(BUILD_PREBUILT)
+EOF
+done
