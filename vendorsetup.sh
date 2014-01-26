a27025be558e98a780cc305a226430c70b746d73
Rene Treffer <treffer@measite.de>
2014-01-26 16:45:37 +0100

    fetch-apps should run from the base dir

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,8 @@
 add_lunch_combo gofree_arm-eng
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/fetch-apps.sh
+(
+  cd "$DIR"
+  ./fetch-apps.sh
+)
