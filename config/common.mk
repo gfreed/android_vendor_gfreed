e0ef9c56ebc35378a57a354ab46cdbcfbca69f11
Rene Treffer <treffer@measite.de>
2014-02-02 00:00:37 +0100

    Add firefox extensions / preferences

--- config/common.mk
+++ config/common.mk
@@ -15,7 +15,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
   vendor/gofree/prebuilt/bin/sysinit:system/bin/sysinit \
-  vendor/gofree/prebuilt/etc/init.local.rc:root/init.gofree.rc
+  vendor/gofree/prebuilt/etc/init.local.rc:root/init.gofree.rc \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/preferences.json:system/org.mozilla.firefox/distribution/preferences.json \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml:system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/https-everywhere-3.5android.7428d856c71576b9e84e349802d424a4c96098c3.xpi:system/org.mozilla.firefox/distribution/extensions/https-everywhere-3.5android.7428d856c71576b9e84e349802d424a4c96098c3.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/no-google-analytics.xpi:system/org.mozilla.firefox/distribution/extensions/no-google-analytics.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/youtube_object2iframe-1.4.1-fx+an.xpi:system/org.mozilla.firefox/distribution/extensions/youtube_object2iframe-1.4.1-fx+an.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/cacert_root_certificate-0.1-fx+an.xpi:system/org.mozilla.firefox/distribution/extensions/cacert_root_certificate-0.1-fx+an.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/adblock-edge.xpi:system/org.mozilla.firefox/distribution/extensions/adblock-edge.xpi
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/gofree/overlay/common
 
