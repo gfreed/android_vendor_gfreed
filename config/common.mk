0902388f65aba706d92a54a13edaa3a6d0915900
Rene Treffer <treffer@measite.de>
2014-02-02 01:33:26 +0100

    Change xpi names to reflect firefox recommendation

--- config/common.mk
+++ config/common.mk
@@ -18,11 +18,11 @@ PRODUCT_COPY_FILES += \
   vendor/gofree/prebuilt/etc/init.local.rc:root/init.gofree.rc \
   vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/preferences.json:system/org.mozilla.firefox/distribution/preferences.json \
   vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml:system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml \
-  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/https-everywhere-3.5android.7428d856c71576b9e84e349802d424a4c96098c3.xpi:system/org.mozilla.firefox/distribution/extensions/https-everywhere-3.5android.7428d856c71576b9e84e349802d424a4c96098c3.xpi \
-  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/no-google-analytics.xpi:system/org.mozilla.firefox/distribution/extensions/no-google-analytics.xpi \
-  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/youtube_object2iframe-1.4.1-fx+an.xpi:system/org.mozilla.firefox/distribution/extensions/youtube_object2iframe-1.4.1-fx+an.xpi \
-  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/cacert_root_certificate-0.1-fx+an.xpi:system/org.mozilla.firefox/distribution/extensions/cacert_root_certificate-0.1-fx+an.xpi \
-  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/adblock-edge.xpi:system/org.mozilla.firefox/distribution/extensions/adblock-edge.xpi
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/jid1-JcGokIiQyjoBAQ@jetpack.xpi:system/org.mozilla.firefox/distribution/extensions/jid1-JcGokIiQyjoBAQ@jetpack.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/https-everywhere@eff.org.xpi:system/org.mozilla.firefox/distribution/extensions/https-everywhere@eff.org.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/jid1-C86cNe1crgj9bw@jetpack.xpi:system/org.mozilla.firefox/distribution/extensions/jid1-C86cNe1crgj9bw@jetpack.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/youtube-o2i@jetpack.xpi:system/org.mozilla.firefox/distribution/extensions/youtube-o2i@jetpack.xpi \
+  vendor/gofree/prebuilt/system/org.mozilla.firefox/distribution/extensions/{fe272bd1-5f76-4ea4-8501-a05d35d823fc}.xpi:system/org.mozilla.firefox/distribution/extensions/{fe272bd1-5f76-4ea4-8501-a05d35d823fc}.xpi
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/gofree/overlay/common
 
