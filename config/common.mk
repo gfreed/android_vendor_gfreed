eec0b7979f43982daa22aa1cde77d60a4d3b4da7
Rene Treffer <treffer@measite.de>
2014-03-18 23:59:56 +0100

    Use ff searchplugin form

--- config/common.mk
+++ config/common.mk
@@ -19,7 +19,8 @@ PRODUCT_COPY_FILES += \
   vendor/gfreed/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
   vendor/gfreed/prebuilt/etc/init.local.rc:root/init.gfreed.rc \
   vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/preferences.json:system/org.mozilla.firefox/distribution/preferences.json \
-  vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/searchplugins/common/DuckDuckGo.xml:system/org.mozilla.firefox/distribution/searchplugins/common/DuckDuckGo.xml \
+  vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml:system/org.mozilla.firefox/distribution/searchplugins/common/duckduckgo.xml \
+  vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/searchplugins/common/list.txt:system/org.mozilla.firefox/distribution/searchplugins/common/list.txt \
   vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/extensions/jid1-JcGokIiQyjoBAQ@jetpack.xpi:system/org.mozilla.firefox/distribution/extensions/jid1-JcGokIiQyjoBAQ@jetpack.xpi \
   vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/extensions/https-everywhere@eff.org.xpi:system/org.mozilla.firefox/distribution/extensions/https-everywhere@eff.org.xpi \
   vendor/gfreed/prebuilt/system/org.mozilla.firefox/distribution/extensions/jid1-C86cNe1crgj9bw@jetpack.xpi:system/org.mozilla.firefox/distribution/extensions/jid1-C86cNe1crgj9bw@jetpack.xpi \
